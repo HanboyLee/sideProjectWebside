6a6bdec49a90ccb11fd872cdf0ee59b60996ab56
HanboyLee <cdman073@gmail.com>
2021-07-18 16:14:19 +0800

    test2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { Dashborder, Product, About, Template } from './pages';
 import { Navbar } from './components';
 
 function App() {
-    console.log(123);
     return (
         <Router>
             <Navbar routes={meberRouter} />
